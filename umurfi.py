2c2b41026e1e67d8651ecd85e3806d8ac88962f8
Sikunyuk <99116266+rofisikunyuk@users.noreply.github.com>
2022-02-12 00:23:45 +0700

    Update umurfi.py

--- umurfi.py
+++ umurfi.py
@@ -4,10 +4,10 @@ import pyfiglet
 ascii_banner = pyfiglet.figlet_format("Umurfi")
 print(ascii_banner)
 # masukan nama
-a = input("\033[1;31m[+]\033[1;37m Masukan Nama Anda: ")
+a = str(input("\033[1;31m[+]\033[1;37m Masukan Nama Anda: "))
 # Masukan tahun kelahiran
 b = int(input("\033[1;31m[+]\033[1;37m Masukan Tanggal Lahir Anda: "))
-c = input("\033[1;31m[+]\033[1;37m Masukan Bulan Lahir Anda: ")
+c = str(input("\033[1;31m[+]\033[1;37m Masukan Bulan Lahir Anda: "))
 d = int(input("\033[1;31m[+]\033[1;37m Masukan Tahun Lahir Anda: "))
 # Tahun sekrang
 e = 2022
