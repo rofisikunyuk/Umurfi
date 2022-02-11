553ed0188c1551609f766a397bdad0d62b1ac312
Sikunyuk <99116266+rofisikunyuk@users.noreply.github.com>
2022-02-12 00:08:09 +0700

    Add files via upload

---
+++ umurfi.py
@@ -0,0 +1,17 @@
+# Baner
+import pyfiglet
+
+ascii_banner = pyfiglet.figlet_format("Umurfi")
+print(ascii_banner)
+# masukan nama
+a = input("\033[1;31m[+]\033[1;37m  Masukan Nama Anda: ")
+# Masukan tahun kelahiran
+b = int(input("\033[1;31m[+]\033[1;37m  Masukan Tanggal Lahir Anda: "))
+c = input("\033[1;31m[+]\033[1;37m  Masukan Bulan Lahir Anda: ")
+d = int(input("\033[1;31m[+]\033[1;37m  Masukan Tahun Lahir Anda: "))
+# Tahun sekrang
+e = 2022
+# Hasil
+print("\033[1;32m[>]\033[1;37m  Hai",a,"Kamu Lahir Pada Tanggal",b,c,d)
+print("\033[1;32m[>]\033[1;37m  Umur Kamu Sekarang Adalah",e - d,"Tahun.")
+print("\n")
